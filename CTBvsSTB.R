32654a112089116a8cb1494303babb350dd0a31d
JBogias <konstantinos.bogias@adelaide.edu.au>
2018-07-09 16:15:31 +0930

    Added code to get the counts-per-million per gene. However, at this stage I am trying to use the dendogram functions. I had trouble with this last year and so I will need to run that stage on phoenix to avoid frying my mac. The vector memory is exhausted.

--- CTBvsSTB.R
+++ CTBvsSTB.R
@@ -2,7 +2,12 @@ library(edgeR)
 library(limma)
 library(magrittr)
 library(dplyr)
+library(readr)
+library(ggplot2)
 library(tibble)
+library(RColorBrewer)
+library(DESeq)
+library(splitstackshape)
 
 # Define the sub-directory
 subdir <- "~/Documents/PhD/Data/counts"
@@ -45,10 +50,59 @@ counts_matrix <- dplyr::select(counts_df, ends_with("Counts"))
 indx <- sapply(counts_matrix, is.factor)
 counts_matrix[indx] <- lapply(counts_matrix[indx], function(x) as.numeric(as.character(x)))
 
-# Now create the DGEList!
+# Now create the countsDGE!
 countsDGE <- DGEList(counts_matrix)
 
+# fit the gene expression to a linear model
+names <- c(currentVar, "Cell_type")
+samples <- c("FemCTB", "FemSTB", "MCTB", "MSTB")
+FemCTB_meta <- c("Female", "Cytotrophoblast")
+FemSTB_meta <- c("Female", "Syncytiotrophoblast")
+MCTB <- c("Male", "Cytotrophoblast")
+MSTB <- c("Male", "Syncytiotrophoblast")
 
+# Create the metadata
+metadata <- rbind(FemCTB_meta, FemSTB_meta, MCTB, MSTB)
+rownames(metadata) <- samples
+colnames(metadata) <- names
+metadata <- as.data.frame(metadata)
 
+currentVar <- "Sex"
 
+# This ias a little convenient function to get the design matrix
+getDesignMatrix <- function(variable, metadata) {
+  numericVar <- is.numeric(metadata[[currentVar]])
+  int <- dplyr::if_else(numericVar, 1, 0)
+  as.formula(paste("~", paste(int, currentVar, sep = "+"))) %>%
+             model.matrix(data = metadata)
+}
 
+designMatrix <- getDesignMatrix(currentVar, metadata = metadata)
+
+fitGeneExpr <- limma::voom(countsDGE, designMatrix, plot = FALSE) %>%
+  limma::lmFit(design = designMatrix) %>%
+  limma::eBayes()
+nGenesTotal <- nrow(fitGeneExpr)
+slopeContrast <- colnames(fitGeneExpr$design)[2]
+topGenes <- limma::topTable(fitGeneExpr,
+                            coef = slopeContrast, number = nGenesTotal) %>%
+  tibble::rownames_to_column("GeneID")
+
+## Define gene and sample order to subset for in the topGenes object
+inputVals <- metadata[[currentVar]]
+sampleOrder <- order(inputVals)
+
+GeneOrder <- order(topGenes$GeneID)
+DE_Genes <- topGenes[GeneOrder, ] %>%
+  tibble::as_data_frame()
+
+### Select only for FDR and gene names
+
+geneExprPValFull <- dplyr::select(DE_Genes, .data$GeneID, .data$P.Value)
+
+## Retireve Gene IDs for all samples
+#First we want to get the gene names and FDR for all of the samples, this way we can make an FDR bar that will give a colour indication of the FDRcutoff
+
+topGenesTotal <- rownames(limma::topTable(fitGeneExpr, coef = slopeContrast, number = nGenesTotal))
+inputValsTotal <- metadata[[currentVar]]
+sampleOrderTotal <- order(inputValsTotal)
