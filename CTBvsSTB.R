25e57a78eed7817675f5c54d101a3dd607460578
JBogias <konstantinos.bogias@adelaide.edu.au>
2018-07-09 14:32:48 +0930

    Added some comments to the code

--- CTBvsSTB.R
+++ CTBvsSTB.R
@@ -1,11 +1,14 @@
+# Define the sub-directory
 subdir <- "~/Documents/PhD/Data/counts"
 
+#Run through each of the count files
 countFiles <- list.files(
   subdir,
   pattern=".counts",
   full.names = TRUE
 )
 
+# A little nifty way to make a data frame from all of your files
 counts <- countFiles %>%
   lapply(function(x){
     read.delim(x, 
@@ -14,21 +17,30 @@ counts <- countFiles %>%
       set_names(x)}) %>%
   as.data.frame()
 
+#Dont worry about this bad code, it was me being a fool. I wanted to easily make the new column names for my data frame
 #new_colnames <- counts[2,]
 
+# The first two lines don't contain any proper data, but they do contain info on the colnames
 counts_df <- counts[-c(1:2),]
 
+# Manually make the new colnames, if theres and easier way, I'm all ears
 manual_colnames <- c("FemCTB_Chr", "FemCTB_Start", "FemCTB_End", "FemCTB_Strand", "FemCTB_Length", "FemCTB_Counts",
                      "FemSTB_Chr", "FemSTB_Start", "FemSTB_End", "FemSTB_Strand", "FemSTB_Length", "FemSTB_Counts",
                      "MCTB_Chr", "MCTB_Start", "MCTB_End", "MCTB_Strand", "MCTB_Length", "MCTB_Counts",
                      "MSTB_Chr", "MSTB_Start", "MSTB_End", "MSTB_Strand", "MSTB_Length", "MSTB_Counts")
 
+# Add in the new colnames
 colnames(counts_df) <- manual_colnames
 
+# This code selects for the columns which contain the gene counts, the DGElist only wants gene count data, doesn't like anything non-numeric
 counts_matrix <- dplyr::select(counts_df, ends_with("Counts"))
 
-countsDGE <- DGEList(counts_matrix)
-
+# Changes the inputs from factors to numeric
 indx <- sapply(counts_matrix, is.factor)
 counts_matrix[indx] <- lapply(counts_matrix[indx], function(x) as.numeric(as.character(x)))
+
+# Now create the DGEList!
+countsDGE <- DGEList(counts_matrix)
+
+
 
